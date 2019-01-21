fae428c6b578924d0d76a8612dbd7dc494479182
Gil Pratte <gil.pratte@mutualmobile.com>
2019-01-21 06:37:45 -0600

    set the admin password in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,5 @@ drop table if exists user;
 
 create table user (id bigint not null auto_increment, username varchar(255), password varchar(255), primary key (id));
 
--- password is password1
-INSERT INTO user (id, username, password) VALUES (1, 'admin1', '$2a$04$Ye7/lJoJin6.m9sOJZ9ujeTgHEVM4VXgI2Ingpsnf9gXyXEXf/IlW');
+-- password is password
+INSERT INTO user (id, username, password) VALUES (1, 'admin', '$2a$10$qXQo4z4oXKPEKyYO7bAQmOQ9PhIcHK4LOo/L1U9j/xkLEmseLWECK');
